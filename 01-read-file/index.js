062a026fcf8f2482bdfe2a49b0a912971ae82940
AksanaBond <oksana@MacBook-Air-Oksana.local>
2025-01-15 15:02:05 +0100

    feat: pass 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'text.txt');
+const readableStream = fs.createReadStream(filePath);
+readableStream.pipe(process.stdout);
