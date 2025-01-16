e94ff740c7005e08dc5723060293e4f9e3aad56a
AksanaBond <oksana@MacBook-Air-Oksana.local>
2025-01-16 21:53:04 +0100

    feat: pass 03-files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+async function displayInfo() {
+  try {
+    const folderPath = path.join(__dirname, 'secret-folder');
+    const files = await fs.readdir(folderPath, { withFileTypes: true });
+    for (const file of files) {
+      if (!file.isFile()) continue;
+      const filepath = path.join(folderPath, file.name);
+      const stats = await fs.stat(filepath);
+      const fileName = path.parse(file.name).name;
+      const fileExtname = path.extname(file.name).slice(1);
+      const fileSize = `${(stats.size / 1024).toFixed(3)}kb`;
+      console.log(`${fileName} - ${fileExtname} - ${fileSize}`);
+    }
+  } catch (error) {
+    console.error('Error reading folder:', error.message);
+  }
+}
+displayInfo();
